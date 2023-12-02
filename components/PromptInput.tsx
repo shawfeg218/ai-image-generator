f4a8ded1f7f77d8afb3fb12100c7a4fe881800eb
shawn <shawnfeng610@gmail.com>
2023-12-03 03:25:53 +0800

    add PromptInput ui

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,10 +1,46 @@
+"use client";
+import { useState } from "react";
 import { Textarea } from "./ui/textarea";
 
 export default function PromptInput() {
+  const [input, setInput] = useState("");
   return (
-    <div>
-      PromptInput
-      <Textarea className="resize-none" />
+    <div className="m-10">
+      <form
+        className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x"
+        action=""
+      >
+        <Textarea
+          placeholder="Enter a prompt..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="resize-none"
+        />
+        <button
+          type="submit"
+          className={`p-4 font-bold ${
+            input
+              ? "bg-slate-950 text-white transition-colors duration-200"
+              : "text-gray-300 cursor-not-allowed"
+          }`}
+          disabled={!input}
+        >
+          Generate
+        </button>
+        <button
+          type="button"
+          className="p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:bg-gray-400"
+        >
+          Use Suggestion
+        </button>
+        <button
+          type="button"
+          className="p-4 bg-white text-violet-500 transition-colors duration-200 font-bold border-none 
+        md:rounded-r-md md:rounded-bl-none"
+        >
+          New Suggestion
+        </button>
+      </form>
     </div>
   );
 }
