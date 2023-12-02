a0d1a69c5c955301908a2fc360e9eb31fc91aff3
shawn <shawnfeng610@gmail.com>
2023-12-03 02:56:48 +0800

    implement shadcn-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PromptInput from "@/components/PromptInput";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
 
         {/* prompt input */}
+        <PromptInput />
 
+        {/* main */}
         {children}
       </body>
     </html>
---
+++ components/PromptInput.tsx
@@ -0,0 +1,10 @@
+import { Textarea } from "./ui/textarea";
+
+export default function PromptInput() {
+  return (
+    <div>
+      PromptInput
+      <Textarea className="resize-none" />
+    </div>
+  );
+}
