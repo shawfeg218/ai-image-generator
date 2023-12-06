5fe818cf7a51f2a75b30aff5b5584e140134bb32
shawn <shawnfeng610@gmail.com>
2023-12-06 10:50:55 +0000

    force-dynamic in page.tsx getImage with access token

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Images from "@/components/Images";
 import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 900;
 
 export default function Home() {
   return (
