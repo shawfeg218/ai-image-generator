163af863c4c349e6262ee25b8286a3f3776d9a83
shawn <shawnfeng610@gmail.com>
2023-12-25 15:08:22 +0000

    use route segment config in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Images from "@/components/Images";
 import { Suspense } from "react";
 
 // export const dynamic = "force-dynamic";
-export const revalidate = 30;
+export const revalidate = 0;
 
 export default function Home() {
   return (
